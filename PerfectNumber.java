99ac702d1e89b29c3d8081499507514f6de59a02
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-10-27 23:58:24 +0530

    Optimized

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -10,9 +10,31 @@ class PerfectNumber {
         }
         return sum==num;
     }
+
+
+    public static boolean checkPerfectNumber2(int num) {
+        if(num<=1) return false;
+        int sum=1;// 1 always a divisor
+        for(int i=2;i*i<=num;i++)
+        {
+                 if(num%i==0)
+                 {
+                    sum+=i;
+                    if(i!=num/i)
+                    {
+                        sum+=num/i;
+                    }
+                 }
+        }
+    
+        
+        return sum==num;
+    }
+
     public static void main(String[] args) {
         PerfectNumber n = new PerfectNumber();
         System.out.println(n.checkPerfectNumber(21));
+        System.err.println(checkPerfectNumber2(21));
     }
    
    
