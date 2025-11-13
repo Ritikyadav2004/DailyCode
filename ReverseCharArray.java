36bcfe217aa7a349eccbd18a0c98f8a8ac222ee9
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-11-13 22:34:56 +0530

    13-11-2025

---
+++ ReverseCharArray.java
@@ -0,0 +1,23 @@
+public class ReverseCharArray {
+    public static void main(String[] args) {
+        char ch[] = {'h','e','l','l','o'};
+        int n=ch.length-1;
+        for(char c:ch)
+        {
+            System.out.print(c+" ");
+        }
+        System.err.println("\nReveresed array");
+        for(int i=0;i<ch.length/2;i++)
+        {
+            char temp =ch[i];
+            ch[i] = ch[n-i];
+            ch[n-i]=temp;
+
+        }
+
+        for(char c:ch)
+        {
+            System.out.print(c+" ");
+        }
+    }
+}
