dc440967ffab8c0a63c24731ea9babfba5c8be2d
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-11-16 23:56:20 +0530

    16-11-2025

--- returnIndex_firstUnique.java
+++ returnIndex_firstUnique.java
@@ -23,7 +23,16 @@ public class returnIndex_firstUnique {
         {
             map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0 )+1);
         }
-        
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(map.get(s.charAt(i))==1)
+            {
+            System.out.println(i);
+            break;
+            }
+        }
+
 
         
     }
