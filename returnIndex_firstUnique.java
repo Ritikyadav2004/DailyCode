4b237247e3031a0a495c5d6693d2ac772442fa86
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-11-16 23:54:22 +0530

    16-11-2025

---
+++ returnIndex_firstUnique.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class returnIndex_firstUnique {
+    public static void main(String[] args) {
+        String s = "Leetcode";
+        int count[] = new int[256];
+        for(int i=0;i<s.length();i++)
+        {
+            count[s.charAt(i)]++;
+        }
+
+         for(int i=0;i<s.length();i++)
+        {
+            if(count[s.charAt(i)]==1)
+            {
+                System.out.println(i);
+                break;
+            }
+        }
+
+        // other method
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<s.length();i++)
+        {
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0 )+1);
+        }
+        
+
+        
+    }
+}
