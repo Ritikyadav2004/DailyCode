bef0f61b74c4cda739b4bef871261bf47ee58612
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-11-28 23:43:16 +0530

    28-11-2025

---
+++ Codeforces/TwoIntervals.java
@@ -0,0 +1,30 @@
+//
+// Given the boundaries of 2 intervals. Print the boundaries of their intersection.
+
+// Note: Boundaries mean the two ends of an interval which are the starting number and the ending number.
+
+// Input
+// Only one line contains two intervals [l1,r1]
+// , [l2,r2]
+//  where (1≤l1,l2,r1,r2≤109)
+// , (l1≤r1,l2≤r2)
+// .
+
+// It's guaranteed that l1≤r1
+//  and l2≤r2
+// .
+
+// Output
+// If there is an intersection between these 2 intervals print its boundaries , otherwise print -1.
+
+// Examples
+// InputCopy
+// 1 15 5 27
+// OutputCopy
+// 5 15
+// //
+package Codeforces;
+
+public class TwoIntervals {
+    
+}
