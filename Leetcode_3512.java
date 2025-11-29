e99e7d80ac01b465c27b87ebf7538ab9777d60ea
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-11-29 23:16:11 +0530

    29-11-2025

---
+++ Leetcode_3512.java
@@ -0,0 +1,22 @@
+public class Leetcode_3512 {
+    
+    public  static int minOperations(int[] nums, int k) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            sum+=nums[i];
+        }
+        int count=0;
+        while(sum%k!=0)
+        {
+            sum--;
+            count++;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] arr={3,9,7};
+        System.out.println(minOperations(arr, 5));
+    }
+
+}
