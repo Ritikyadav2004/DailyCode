3219f3ea9a4e530f3f2d2d8d4f7a9530138730d6
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-12-04 23:45:43 +0530

    04-12-2025

---
+++ Codeforces/Sheet_2/A_oneToN.java
@@ -0,0 +1,14 @@
+package Codeforces.Sheet_2;
+import java.util.Scanner;
+public class A_oneToN {
+ public static void main(String[] args)
+ {
+  Scanner sc= new Scanner(System.in);
+  int n = sc.nextInt();
+  for(int i=1;i<=n;i++)
+  {
+   System.out.println(i);
+  }
+  sc.close();
+ }
+}
