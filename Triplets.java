85fedda97763a0de0e52b55fc5f2757eaace46e0
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-12-09 00:02:16 +0530

    08-12-2025

---
+++ Triplets.java
@@ -0,0 +1,21 @@
+public class Triplets {
+    
+    public static  int countTriples(int n) {
+        int count=0;
+        for (int a = 1; a <=n; a++) {
+            for (int b = 1; b <=n; b++) {
+                int c2= a*a + b*b;
+                int c =(int)Math.sqrt(c2);
+                if(c<=n && c*c==c2)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+
+        
+}
+public static void main(String[] args) {
+    System.out.println(countTriples(6));
+}}
