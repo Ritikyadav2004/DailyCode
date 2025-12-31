dadfe0052298792b9438461abcdc5c59ae71f77e
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-12-31 23:54:57 +0530

    31-12-2025

--- Codeforces/Sheet_3/B_Searching.java
+++ Codeforces/Sheet_3/B_Searching.java
@@ -8,35 +8,13 @@ public class B_Searching {
         int arr[] = new int[n];
         boolean flag=true;
 
-       for (int i = 0; i < arr.length; i++) {
+       for (int i = 0; i < n; i++) {
         arr[i]=sc.nextInt();
        }
-    //    Arrays.sort(arr);
+
        int target=sc.nextInt();
 
 
-        //  Binery  Search
-    //    int l=0;
-    //    int h=n-1;
-    //    while(l<=h)
-    //    {
-    //     int mid = l + (h-l)/2;
-    //     if(arr[mid]==target)
-    //     {     
-    //         flag=false;
-    //           System.out.println(mid);
-    //           break;
-    //     }
-    //     if(arr[mid]<target)
-    //     {
-    //         l=mid+1;
-    //     }
-    //     else{
-    //         h=mid-1;
-    //     }
-
-    //    }
-    //    if(flag) System.out.println(-1);
     for (int i = 0; i < arr.length; i++) {
         if(arr[i]==target)
         {
