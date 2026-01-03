633a422f16706a4a596b18cd6b96b426c25c30a1
Ritikyadav2004 <ritikyadav5000@gmail.com>
2026-01-04 00:00:27 +0530

    03-01-26

---
+++ Codeforces/Sheet_3/E_Lowest_Number.java
@@ -0,0 +1,27 @@
+package Codeforces.Sheet_3;
+
+import java.util.Scanner;
+
+public class E_Lowest_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int min=Integer.MAX_VALUE;
+        int arr[]=new int[n];
+        for (int i = 0; i < n; i++) {
+
+            arr[i]=sc.nextInt();
+            min=Math.min(min,arr[i]);
+            
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==min)
+            {
+                System.out.println(arr[i]+" "+i);
+            }
+        }
+        sc.close();
+    }
+}
